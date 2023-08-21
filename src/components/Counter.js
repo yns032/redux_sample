361819d49bbe07b4d453e2bf3230f09bcd52f0dc
yns032 <ya.aslan.44@gmail.com>
2023-08-21 15:49:44 +0300

    add counter

---
+++ src/components/Counter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { UseSelector, useSelector } from "react-redux";
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  return <div>{count}</div>;
+}
+
+export default Counter;
